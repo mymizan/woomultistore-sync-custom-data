854dc021a266fe509904ee95ddc0dfc228da6e44
M Yakub Mizan <ym@orchestra.ltd>
2020-02-13 22:55:14 +0600

    updated whitelist.

---
+++ woomultistore-sync-custom-data.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * WooCommerce Multistore Sync Custom Metadata Add-on
+ *
+ * This is a compability plugin to sync metadata created by Wholesale Price plugin for WooCommerce
+ *
+ * @link              https://woomultistore.com/
+ * @since             1.0.0
+ * @package           Woocommerce_Multistore_Usermeta_Sync
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WooCommerce Multistore Sync Custom Metadata Add-on
+ * Plugin URI:        https://woomultistore.com
+ * Description:       This is a compability plugin to sync user-defined metadata
+ * Version:           1.0.0
+ * Author:            Lykke Media AS
+ * Author URI:        https://woomultistore.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       woocommerce-multistore-usermeta-sync
+ * Domain Path:       /languages
+ * Network: true
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Whitelisted metadata
+ *
+ * Maintain a list of metadata that should be synced automatically
+ *
+ * @package    Woocommerce_Multistore_Usermeta_Sync
+ * @subpackage Woocommerce_Multistore_Usermeta_Sync/includes
+ * @author     Lykke Media AS <a@gmail.com>
+ */
+class Woocommerce_Multistore_Sync_Custom_Meta {
+
+	/**
+	 * The array of whitelisted metadata
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      array    $whitelist    The array of whitelisted metadata for automatic syncing.
+	 */
+	protected $whitelist;
+
+
+	/**
+	 * Initialize the collections used to maintain the actions and filters.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+		$this->whitelist = array(
+			'stock_hengelo',
+			'stock_deventer',
+		);
+
+		add_filter('WOO_MSTORE_admin_product/slave_product_meta_to_update', array($this, 'sync_whitelist'), PHP_INT_MAX, 2);
+	}
+
+	/**
+	 * Syncs the whitelisted metadata
+	 *
+	 * @since    1.0.0
+	 * @return  array array of metada key and meta value.
+	 */
+	public function sync_whitelist($meta_data, $data) {
+		foreach( $this->whitelist as $whitelisted_metakey )
+		{
+			$meta_value = $data['master_product']->get_meta($whitelisted_metakey, true); 
+			$meta_data[ $whitelisted_metakey ] = $meta_value;
+		}
+
+		return $meta_data;
+	}
+}
+
+new Woocommerce_Multistore_Sync_Custom_Meta();
